d95061e191f7705ce48bb435fbf14239fa6d9634
SevenErol <lorenzo.maggi1998@gmail.com>
2023-01-10 17:19:43 +0100

    index function controller connected

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -1,6 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
 
 use App\Models\Project;
 use App\Http\Requests\StoreProjectRequest;
@@ -15,7 +17,9 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        //
+        $projects = Project::all();
+
+        return view('admin.index', compact('projects'));
     }
 
     /**
