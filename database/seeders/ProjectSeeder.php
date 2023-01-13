d0e778492b2d3e83dfc752733692d807204411dc
SevenErol <lorenzo.maggi1998@gmail.com>
2023-01-13 14:48:34 +0100

    modified migartion seeder and config file

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -18,6 +18,7 @@ class ProjectSeeder extends Seeder
     {
         for ($i = 0; $i < 10; $i++) {
             $project = new Project();
+            $project->cover_image = 'placeholders/' . $faker->image('storage/app/public/placeholders', 600, 300, 'Project', false, false);
             $project->title = $faker->sentence(3);
             $project->description = $faker->text();
             $project->date = $faker->date();
